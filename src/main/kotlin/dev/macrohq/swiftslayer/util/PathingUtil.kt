ce5260d62fc0543014150bdb8333327fc887e494
TomJuri <mail@tomjuri.de>
2023-08-28 21:46:58 +0200

    pushing so i can continue on my laptop

--- src/main/kotlin/dev/macrohq/swiftslayer/util/PathingUtil.kt
+++ src/main/kotlin/dev/macrohq/swiftslayer/util/PathingUtil.kt
@@ -2,10 +2,17 @@ package dev.macrohq.swiftslayer.util
 
 import cc.polyfrost.oneconfig.utils.dsl.runAsync
 import dev.macrohq.swiftslayer.pathfinding.AStarPathfinder
+import net.minecraft.entity.EntityLiving
 import net.minecraft.util.BlockPos
 
 object PathingUtil {
+
+    var isDone = true
+      private set
+
     fun goto(pos: BlockPos) {
+        if(!isDone) return
+        isDone = false
         runAsync {
             RenderUtil.lines.clear()
             val path = AStarPathfinder(player.getStandingOn(), pos).findPath(10000)
@@ -15,7 +22,27 @@ object PathingUtil {
                 path.forEach { RenderUtil.lines.add(it) }
                 pathExecutor.executePath(path)
             }
+            while(pathExecutor.running) Thread.sleep(1)
+            isDone = true
         }
     }
-    fun hasArrived() = !pathExecutor.running && pathExecutor.done
+
+    private var isDoneFollowing = true
+    fun follow(entity: EntityLiving) {
+        if(!isDone) return
+        isDone = false
+        isDoneFollowing = false
+        runAsync {
+            while(!isDoneFollowing) {
+                if(!pathExecutor.running)
+                    goto(entity.getStandingOn())
+            }
+        }
+    }
+
+    fun stop() {
+        isDone = true
+        pathExecutor.disable()
+    }
+
 }
